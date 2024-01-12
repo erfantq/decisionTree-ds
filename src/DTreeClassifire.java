1b59524c2f2eddefc271dad5002b5f74600f99b0
erfantq <erfantaghavi319@gmail.com>
2024-01-12 22:53:13 +0330

    let's get started

---
+++ src/DTreeClassifire.java
@@ -0,0 +1,13 @@
+public class DTreeClassifire {
+    public DTreeClassifire(float[][] data, float[] labels){
+        //todo implementation
+    }
+    float predict(float[][] properties, int depth){
+        //todo implementation
+        return 0.0f;
+    }
+    float accuracy(int[] labels, int[] labelsPredicted){
+        //todo implementation
+        return 0.0f;
+    }
+}
---
+++ src/Tree.java
@@ -0,0 +1,18 @@
+public class Tree {
+    private int depth;
+    public int getDepth(){
+        return depth;
+    }
+    public void creatTree(float[][] data, float[] labels){
+        //todo implementation
+    }
+    public float entropy(float[] labels){
+        //todo implementation
+        return 0.0f;
+    }
+    public float iGain(int pEntropy, float[] weight, float[] entropies){
+        //todo implementation
+        return 0.0f;
+    }
+
+}
