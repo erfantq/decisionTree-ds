77d728879c563a76771a675336863fae4127ae20
erfantq <erfantaghavi319@gmail.com>
2024-01-14 17:53:11 +0330

    accuracy method and DTreeClassifire constructor completed

--- src/DTreeClassifire.java
+++ src/DTreeClassifire.java
@@ -1,13 +1,23 @@
 public class DTreeClassifire {
+    private float[][] data;
+    private float[] labels;
     public DTreeClassifire(float[][] data, float[] labels){
-        //todo implementation
+        this.data = data;
+        this.labels = labels;
     }
-    float predict(float[][] properties, int depth){
+    float predict(float[][] data, int depth){
         //todo implementation
         return 0.0f;
     }
     float accuracy(int[] labels, int[] labelsPredicted){
-        //todo implementation
-        return 0.0f;
+        int countCorrectLabels = 0;
+        float result = 0.0f;
+        for (int i = 0; i < labels.length; i++) {
+            if (labels[i] == labelsPredicted[i]){
+                countCorrectLabels++;
+            }
+        }
+        result = (countCorrectLabels / labelsPredicted.length) * 100;
+        return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Tree t = new Tree();
-        float[] test = {0,0,1,1};
-        System.out.println(t.entropy(test));
+//        Tree t = new Tree();
+//        float[] test = {0,0,1,1};
+//        System.out.println(t.entropy(test));
     }
 }
